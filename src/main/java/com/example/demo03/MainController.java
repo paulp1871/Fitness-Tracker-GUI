86ae97942b077ca7e21858e8bfa628d4c4c6f771
vini <vini.godoydemelo@ucalgary.ca>
2025-04-10 00:10:31 -0600

    setting up controller

--- src/main/java/com/example/demo03/MainController.java
+++ src/main/java/com/example/demo03/MainController.java
@@ -8,164 +8,166 @@ import javafx.scene.control.TextArea;
 import javafx.scene.control.TextField;
 import javafx.scene.layout.VBox;
 
-@FXML
-private Button addBicepsSize;
+public class MainController {
 
-@FXML
-private Label addNewEntryLabel;
+    @FXML
+    private Button addBicepsSize;
 
-@FXML
-private VBox addNewEntryPane;
+    @FXML
+    private Label addNewEntryLabel;
 
-@FXML
-private Button addWaistSize;
+    @FXML
+    private VBox addNewEntryPane;
 
-@FXML
-private Button addWeight;
+    @FXML
+    private Button addWaistSize;
 
-@FXML
-private Button chestSize;
+    @FXML
+    private Button addWeight;
 
-@FXML
-private TextArea clientInfoPane;
+    @FXML
+    private Button chestSize;
 
-@FXML
-private TextField existingClientName;
+    @FXML
+    private TextArea clientInfoPane;
 
-@FXML
-private Label functionsLabel;
+    @FXML
+    private TextField existingClientName;
 
-@FXML
-private VBox functionsPane;
+    @FXML
+    private Label functionsLabel;
 
-@FXML
-private TextField newClientName;
+    @FXML
+    private VBox functionsPane;
 
-@FXML
-private Button printBMITrend;
+    @FXML
+    private TextField newClientName;
 
-@FXML
-private Button printBiceps;
+    @FXML
+    private Button printBMITrend;
 
-@FXML
-private Button printCalIntake;
+    @FXML
+    private Button printBiceps;
 
-@FXML
-private Button printChest;
+    @FXML
+    private Button printCalIntake;
 
-@FXML
-private VBox printPastEntriesPane;
+    @FXML
+    private Button printChest;
 
-@FXML
-private Label printPastEntryLabel;
+    @FXML
+    private VBox printPastEntriesPane;
 
-@FXML
-private Button printWaist;
+    @FXML
+    private Label printPastEntryLabel;
 
-@FXML
-private Button printWeeklyCalIntake;
+    @FXML
+    private Button printWaist;
 
-@FXML
-private Button printWeight;
+    @FXML
+    private Button printWeeklyCalIntake;
 
-@FXML
-private Button printWeightTrend;
+    @FXML
+    private Button printWeight;
 
-@FXML
-private Label statusMessage;
+    @FXML
+    private Button printWeightTrend;
 
-@FXML
-void About(ActionEvent event) {
+    @FXML
+    private Label statusMessage;
 
-}
+    @FXML
+    void About(ActionEvent event) {
 
-@FXML
-void Load(ActionEvent event) {
+    }
 
-}
+    @FXML
+    void Load(ActionEvent event) {
 
-@FXML
-void SaveAs(ActionEvent event) {
+    }
 
-}
+    @FXML
+    void SaveAs(ActionEvent event) {
 
-@FXML
-void addBicepsSize(ActionEvent event) {
+    }
 
-}
+    @FXML
+    void addBicepsSize(ActionEvent event) {
 
-@FXML
-void addChestSize(ActionEvent event) {
+    }
 
-}
+    @FXML
+    void addChestSize(ActionEvent event) {
 
-@FXML
-void addWaistSize(ActionEvent event) {
+    }
 
-}
+    @FXML
+    void addWaistSize(ActionEvent event) {
 
-@FXML
-void addWeight(ActionEvent event) {
+    }
 
-}
+    @FXML
+    void addWeight(ActionEvent event) {
 
-@FXML
-void compareClientsByHeight(ActionEvent event) {
+    }
 
-}
+    @FXML
+    void compareClientsByHeight(ActionEvent event) {
 
-@FXML
-void createNewClient(ActionEvent event) {
+    }
 
-}
+    @FXML
+    void createNewClient(ActionEvent event) {
 
-@FXML
-void printAllClients(ActionEvent event) {
+    }
 
-}
+    @FXML
+    void printAllClients(ActionEvent event) {
 
-@FXML
-void printBMITrend(ActionEvent event) {
+    }
 
-}
+    @FXML
+    void printBMITrend(ActionEvent event) {
 
-@FXML
-void printBiceps(ActionEvent event) {
+    }
 
-}
+    @FXML
+    void printBiceps(ActionEvent event) {
 
-@FXML
-void printCalIntake(ActionEvent event) {
+    }
 
-}
+    @FXML
+    void printCalIntake(ActionEvent event) {
 
-@FXML
-void printChest(ActionEvent event) {
+    }
 
-}
+    @FXML
+    void printChest(ActionEvent event) {
 
-@FXML
-void printExistingClient(ActionEvent event) {
+    }
 
-}
+    @FXML
+    void printExistingClient(ActionEvent event) {
 
-@FXML
-void printWaist(ActionEvent event) {
+    }
 
-}
+    @FXML
+    void printWaist(ActionEvent event) {
 
-@FXML
-void printWeeklyCalIntake(ActionEvent event) {
+    }
 
-}
+    @FXML
+    void printWeeklyCalIntake(ActionEvent event) {
 
-@FXML
-void printWeight(ActionEvent event) {
+    }
 
-}
+    @FXML
+    void printWeight(ActionEvent event) {
 
-@FXML
-void printWeightTrend(ActionEvent event) {
+    }
 
+    @FXML
+    void printWeightTrend(ActionEvent event) {
 
+    }
 }
