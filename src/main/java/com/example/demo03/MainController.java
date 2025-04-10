624eb984249d2bd0fb6013286b5f34e69a68a526
vini <vini.godoydemelo@ucalgary.ca>
2025-04-10 00:06:50 -0600

    setting up controller

---
+++ src/main/java/com/example/demo03/MainController.java
@@ -0,0 +1,171 @@
+package com.example.demo03;
+
+import javafx.fxml.FXML;
+import javafx.scene.control.Label;
+import javafx.event.ActionEvent;
+import javafx.scene.control.Button;
+import javafx.scene.control.TextArea;
+import javafx.scene.control.TextField;
+import javafx.scene.layout.VBox;
+
+@FXML
+private Button addBicepsSize;
+
+@FXML
+private Label addNewEntryLabel;
+
+@FXML
+private VBox addNewEntryPane;
+
+@FXML
+private Button addWaistSize;
+
+@FXML
+private Button addWeight;
+
+@FXML
+private Button chestSize;
+
+@FXML
+private TextArea clientInfoPane;
+
+@FXML
+private TextField existingClientName;
+
+@FXML
+private Label functionsLabel;
+
+@FXML
+private VBox functionsPane;
+
+@FXML
+private TextField newClientName;
+
+@FXML
+private Button printBMITrend;
+
+@FXML
+private Button printBiceps;
+
+@FXML
+private Button printCalIntake;
+
+@FXML
+private Button printChest;
+
+@FXML
+private VBox printPastEntriesPane;
+
+@FXML
+private Label printPastEntryLabel;
+
+@FXML
+private Button printWaist;
+
+@FXML
+private Button printWeeklyCalIntake;
+
+@FXML
+private Button printWeight;
+
+@FXML
+private Button printWeightTrend;
+
+@FXML
+private Label statusMessage;
+
+@FXML
+void About(ActionEvent event) {
+
+}
+
+@FXML
+void Load(ActionEvent event) {
+
+}
+
+@FXML
+void SaveAs(ActionEvent event) {
+
+}
+
+@FXML
+void addBicepsSize(ActionEvent event) {
+
+}
+
+@FXML
+void addChestSize(ActionEvent event) {
+
+}
+
+@FXML
+void addWaistSize(ActionEvent event) {
+
+}
+
+@FXML
+void addWeight(ActionEvent event) {
+
+}
+
+@FXML
+void compareClientsByHeight(ActionEvent event) {
+
+}
+
+@FXML
+void createNewClient(ActionEvent event) {
+
+}
+
+@FXML
+void printAllClients(ActionEvent event) {
+
+}
+
+@FXML
+void printBMITrend(ActionEvent event) {
+
+}
+
+@FXML
+void printBiceps(ActionEvent event) {
+
+}
+
+@FXML
+void printCalIntake(ActionEvent event) {
+
+}
+
+@FXML
+void printChest(ActionEvent event) {
+
+}
+
+@FXML
+void printExistingClient(ActionEvent event) {
+
+}
+
+@FXML
+void printWaist(ActionEvent event) {
+
+}
+
+@FXML
+void printWeeklyCalIntake(ActionEvent event) {
+
+}
+
+@FXML
+void printWeight(ActionEvent event) {
+
+}
+
+@FXML
+void printWeightTrend(ActionEvent event) {
+
+
+}
--- src/main/java/com/example/demo03/MainGUI.java
+++ src/main/java/com/example/demo03/MainGUI.java
@@ -7,12 +7,12 @@ import javafx.stage.Stage;
 
 import java.io.IOException;
 
-public class HelloApplication extends Application {
+public class MainGUI extends Application {
     @Override
     public void start(Stage stage) throws IOException {
-        FXMLLoader fxmlLoader = new FXMLLoader(HelloApplication.class.getResource("hello-view.fxml"));
-        Scene scene = new Scene(fxmlLoader.load(), 320, 240);
-        stage.setTitle("Hello!");
+        FXMLLoader fxmlLoader = new FXMLLoader(MainGUI.class.getResource("main.fxml"));
+        Scene scene = new Scene(fxmlLoader.load(), 700, 900);
+        stage.setTitle("Fitness Tracker");
         stage.setScene(scene);
         stage.show();
     }
